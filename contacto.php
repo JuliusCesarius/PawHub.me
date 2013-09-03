0a4be848232ec7bf417deffa945ec23d7c53065b
JuliuCesarius <michavo2000@gmail.com>
2013-09-03 11:30:41 -0500

    Se agregó el código de Analytics a la página de contacto

--- contacto.php
+++ contacto.php
@@ -403,7 +403,23 @@ if(array_key_exists("login",$_GET)) {
 			
 		</div>
 
-		
+				<script type="text/javascript">
+			(function(i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] ||
+				function() {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+				a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+			ga('create', 'UA-10483060-4', 'pawhub.me');
+			ga('send', 'pageview');
+
+		</script>
 	</body>
 	<script>
 		//Funcion que verifica campos del formulario vacios para IE y Safari
